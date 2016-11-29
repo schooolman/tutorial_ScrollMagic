380687e1634fc48e557e8639bba86133fe0b67e0
Jake Schoolmeesters <j.schoolmee@gmail.com>
2016-11-29 13:17:36 -1000

    finished first section of the ScrollMagic Tutorial

--- basics/01-intro/start/js/main.js
+++ basics/01-intro/start/js/main.js
@@ -1,23 +1,20 @@
 $(document).ready(function(){
 
-	
+//init ScrollMagic
+var controller = new ScrollMagic.Controller();
+
+// build a scene
+var ourScene = new ScrollMagic.Scene({
+	triggerElement: '#project01'
+})
+
+.setClassToggle('#project01', 'fade-in')// add class to project01
+.addIndicators({
+	name: 'fade scene',
+	colorTrigger: 'black',
+	indent: 200,
+	colorStart: 'rgb(237, 176, 57)'
+}) // This adds indicators to the page used for debugging.
+.addTo(controller);
 
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
