c4dcf55b9d1a36c6bd736545430ff099576b04ed
Jake Schoolmeesters <j.schoolmee@gmail.com>
2016-11-29 17:18:36 -1000

    finished up unit 3 of the scrollermagic tutorial. Good so far.

--- basics/01-intro/start/js/main.js
+++ basics/01-intro/start/js/main.js
@@ -3,20 +3,28 @@ $(document).ready(function(){
 //init ScrollMagic
 var controller = new ScrollMagic.Controller();
 
-// build a scene
-var ourScene = new ScrollMagic.Scene({
-	triggerElement: '#project01 img', // by adding the img tag it moves the trigger element to the top of the img tag.
-	duration: '90%', //Will work for responsive sizes
-	triggerHook: 0.9
-})
 
-.setClassToggle('#project01', 'fade-in')// add class to project01
-.addIndicators({
-	name: 'fade scene',
-	colorTrigger: 'black',
-	colorStart: 'rgb(237, 176, 57)',
-	colorEnd: 'rgb(237, 57, 214)'
-}) // This adds indicators to the page used for debugging.
-.addTo(controller);
+
+// loop through each .project element
+$('.project').each(function(){
+
+	// build a scene
+	var ourScene = new ScrollMagic.Scene({
+		triggerElement: this.children[0], // by adding the img tag it moves the trigger element to the top of the img tag.
+		// duration: '90%', //Will work for responsive sizes
+		triggerHook: 0.9,
+		// reverse: false //makes the animation only happen one time. Won't re-trigger on scroll.
+	})
+
+	.setClassToggle(this, 'fade-in')// add class to project01
+	.addIndicators({
+		name: 'fade scene',
+		colorTrigger: 'black',
+		colorStart: 'rgb(237, 176, 57)',
+		colorEnd: 'rgb(237, 57, 214)'
+	}) // This adds indicators to the page used for debugging.
+	.addTo(controller);
+
+});
 
 });
