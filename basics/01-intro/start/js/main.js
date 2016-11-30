01ccc2b6a4d8129ffc5476502e911032d0ee5aba
Jake Schoolmeesters <j.schoolmee@gmail.com>
2016-11-29 17:05:27 -1000

    finishing unit 2 of the scroll magic tutorial.

--- basics/01-intro/start/js/main.js
+++ basics/01-intro/start/js/main.js
@@ -5,15 +5,17 @@ var controller = new ScrollMagic.Controller();
 
 // build a scene
 var ourScene = new ScrollMagic.Scene({
-	triggerElement: '#project01'
+	triggerElement: '#project01 img', // by adding the img tag it moves the trigger element to the top of the img tag.
+	duration: '90%', //Will work for responsive sizes
+	triggerHook: 0.9
 })
 
 .setClassToggle('#project01', 'fade-in')// add class to project01
 .addIndicators({
 	name: 'fade scene',
 	colorTrigger: 'black',
-	indent: 200,
-	colorStart: 'rgb(237, 176, 57)'
+	colorStart: 'rgb(237, 176, 57)',
+	colorEnd: 'rgb(237, 57, 214)'
 }) // This adds indicators to the page used for debugging.
 .addTo(controller);
 
